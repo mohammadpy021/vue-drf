36a943019fac90f968d13736b9f264490f1a2eca
mohammadpy021 <mohammad.py021@gmail.com>
2023-09-09 15:23:39 +0330

    Login validation

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,16 +2,24 @@
   <div class="Login">
     <div class="col-lg-3  mx-auto border shadow rounded p-4 mt-3">
       <h1 class="text-center">Login</h1>
+
+
+
+
+
       <form @submit.prevent="doLogin">
-        <div class="form-group">
+
+        <div class="form-group ">
           <label for="username">Username:</label>
-          <input type="text" class="form-control" id="username" v-model="username">
+          <input type="text" class="form-control" id="username" v-model="username" v-bind:class="{'is-invalid':(usernameE===true), 'is-valid':(usernameE===false)}">
+          <span class="invalid-feedback">{{ usernameEM }}</span>
         </div>
         <div class="form-group">
           <label for="pwd">Password:</label>
-          <input type="password" class="form-control" id="pwd" v-model="password">
+          <input type="password" class="form-control" id="pwd" v-model="password" v-bind:class="{'is-invalid':(passwordE===true), 'is-valid':(passwordE===false)}">
+          <span class="invalid-feedback">{{passwordEM}}</span>
         </div>
-        <button type="submit" class="btn btn-success mt-2" >Submit</button>
+        <button type="submit" class="btn btn-success mt-2">Submit</button>
         <!-- <button type="submit" class="btn btn-success mt-2" @click="doLogin">Submit</button> -->
       </form>
     </div>
@@ -36,16 +44,51 @@ export default {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      usernameE: null,   //Error
+      passwordE: null,   //Error   
+      usernameEM: null,    //ErrorMessage
+      passwordEM: null     //ErrorMessage
+
     }
   },
   methods:
   {
     doLogin() {
-      console.log(this.username)
-      console.log(this.password)
-      // this.$store.commit("login", "toeknvalue")
-      // this.$router.push("/profile")
+      let access = true
+      if (this.username.length < 5) {
+        access = false
+        this.usernameE = true
+        if (this.username.length === 0) {
+          this.usernameEM = "Username is required"
+        }
+        else {
+          this.usernameEM = "Username must be atleat 5 characters"
+        }
+      }
+      else {
+        this.usernameE = false
+        this.usernameEM = ''
+      }
+      if (this.password.length < 6) {
+        access = false
+        this.passwordE = true
+        if (this.password.length === 0) {
+          this.passwordEM = "Password is required"
+        }
+        else {
+          this.passwordEM = "Password must be atleat 6 characters"
+        }
+      }
+      else {
+        this.passwordE = false
+        this.passwordEM = ''
+      }
+      // console.log(this.username)
+      if (access) {
+        this.$store.commit("login", `${this.username}:${this.password}`)
+        this.$router.push("/profile")
+      }
     }
   }
 }
