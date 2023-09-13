73d3791e074b41e0d8574122020503a9744bdcdd
mohammadpy021 <mohammad.py021@gmail.com>
2023-09-13 15:34:22 +0330

    set: register api for vue

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -37,7 +37,8 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-JWT = [    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+JWT = [    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     ]
