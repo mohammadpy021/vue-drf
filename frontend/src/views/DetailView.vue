522bd97d1f8a7bc9ce8ee8ff2bac1b2cd46a44ae
mohammadpy021 <mohammad.py021@gmail.com>
2023-09-16 21:41:07 +0330

    update: Edit and Remove of Article

--- frontend/src/views/DetailView.vue
+++ frontend/src/views/DetailView.vue
@@ -11,6 +11,13 @@
           <button type="submit" class="btn btn-danger mb-3 ms-1" @click="doRemove">Remove</button>
         </div>
       </div>
+      <div class="text-center mx-auto alert alert-warning col-lg-10" v-if="errorRemove">
+        Something went wrong durin Deleting
+      </div>
+      <!-- Edit -->
+      <div class="text-center mx-auto alert alert-warning col-lg-10" v-if="errorEdit">
+        Something went wrong durin Editing
+      </div>
       <form class="col-lg-10 mx-auto mb-3 shadow p-4 rounded" @submit.prevent="doEdit" v-if="edit">
         <h3> Add a new post</h3>
         <div class="mb-3">
@@ -46,21 +53,29 @@ export default {
     //   article => article.slug === this.$route.params.slug
     // )
     return {
-      // articles : articles, 
-      // article: article,
       article: {},
       articleNotFound: false,
+      title: "",
+      description: "",
+      content: "",
+      errorEdit: false,
+      errorRemove : false,
+      edit: false
+      // articles : articles, 
+      // article: article,
       // title: article.title,
       // description: article.description,
       // content: article.content,
-      // edit:false
     }
   },
   mounted() {
     axios
       .get(`/articles/${this.$route.params.slug}/`)
       .then((response) => {
-        this.article = response.data
+        this.article = response.data,
+          this.title = this.article.title,
+          this.description = this.article.description,
+          this.content = this.article.content
       })
       .catch((e) => {
         this.articleNotFound = true
@@ -68,31 +83,55 @@ export default {
 
   },
   methods: {
-    // doEdit(){
-    //   let index = this.articles.findIndex(
-    //   article => article.slug === this.$route.params.slug
-    // )
-    // this.articles[index] = {
-    //   title: this.title,
-    //   slug : this.title.replaceAll(" ", "-").toLowerCase(),
-    //   description: this.description,
-    //   content: this.content
-    // }
-    //   let database = JSON.stringify(this.articles)
-    //   localStorage.setItem("articles", database)
-    //   this.article = this.articles[index]
-    //   this.$router.push(`/article/${this.article.slug}`) //redirect to the Edited article page
-    //   this.edit = false
-    // },
-    // doRemove(){
+    doEdit() {
+      let article = {
+        title: this.title,
+        slug : this.title.replaceAll(" ", "-").toLowerCase(),
+        description: this.description,
+        content: this.content
+      }
+      axios
+        .put(`/articles/${this.$route.params.slug}/`, article)
+        .then((response) => {
+          this.edit = false
+          this.article = article
+          this.$router.push(`/article/${article.slug}`) //redirect to the new article page
+        })
+        .catch((e) => {
+          this.errorEdit = true
+        })
+      //   let index = this.articles.findIndex(
+      //   article => article.slug === this.$route.params.slug
+      // )
+      // this.articles[index] = {
+      //   title: this.title,
+      //   slug : this.title.replaceAll(" ", "-").toLowerCase(),
+      //   description: this.description,
+      //   content: this.content
+      // }
+      // let database = JSON.stringify(this.articles)
+      // localStorage.setItem("articles", database)
+      // this.article = this.articles[index]
+      // this.$router.push(`/article/${this.article.slug}`) //redirect to the Edited article page
+      // this.edit = false
+    },
+    doRemove(){
+      axios
+        .delete(`/articles/${this.$route.params.slug}/`)
+        .then((response) => {
+          this.$router.push('/') //redirect to Home page
+        })
+        .catch((e) => {
+          this.errorRemove = true
+        })
     //   let index = this.articles.findIndex(
     //   article => article.slug === this.$route.params.slug
     // )
     //   this.articles.splice(index, 1) // splice wont gieve us "undefined"
     //   let database = JSON.stringify(this.articles)
     //   localStorage.setItem("articles", database)
-    //   this.$router.push('/') //redirect to Home page
-    // }
+      // this.$router.push('/') //redirect to Home page
+    }
   }
 }
 </script>
